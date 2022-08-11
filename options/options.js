0dbdc9002886183d1d828df85c9251435c203ca7
Crowdbotics <team@crowdbotics.com>
2022-08-11 05:13:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "clear_health_pass_36717",
+  url: "https://clear_health_pass_36717.botics.co",
+  api: "https://clear_health_pass_36717.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
